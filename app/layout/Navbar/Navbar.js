e5479f631cce0eb8a0b78e5f8fed9a1d1a0a9258
programmerrafi <mdrafiprogrammer@gmail.com>
2022-11-15 01:28:21 +0600

    navbar almost done

--- app/layout/Navbar/Navbar.js
+++ app/layout/Navbar/Navbar.js
@@ -27,69 +27,81 @@ const Navbar = () => {
     };
   }, []);
   return (
-    <header
-      className={`fixed top-0 z-[10000] w-full ${
-        navbar && "bg-blackSecondary"
-      }`}
-    >
-      <nav className={""}>
-        <div className="container">
-          <div className="flex justify-between items-center w-full py-3">
-            {/* Logo */}
-            <Link href="/">
-              <a href="/">
-                <Image
-                  src="/icons/logo.png"
-                  width="160"
-                  height="55"
-                  objectFit="contain"
-                />
-              </a>
-            </Link>
-            {/* Menu */}
-            <div
-              className="menu lg:hidden cursor-pointer"
-              onClick={() => setOpen((x) => !x)}
-            >
-              <HiOutlineMenuAlt3 size={40} className="text-colorText" />
-            </div>
-
-            {/* NavList */}
-            <div
-              className={`${
-                !open &&
-                "absolute bg-teal-700 left-[-990px] lg:left-0 transition-all duration-200 ease-in-out lg:duration-[0ms] z-[1000]"
-              } absolute top-0 left-0 bg-slate-800 lg:relative lg:top-0 z-[999] lg:justify-between w-screen h-screen lg:h-auto flex-col flex lg:flex lg:w-auto lg:order-1 lg:bg-transparent transition-all duration-700 ease-in-out lg:duration-[0ms]`}
-              id="mobile-menu-4"
-            >
-              {/* ====Menu Close Icon ==== */}
+    <>
+      <header
+        className={`fixed top-0 z-[1005] w-full ${
+          navbar && "bg-blackSecondary"
+        }`}
+      >
+        <nav className={""}>
+          <div className="container">
+            <div className="flex justify-between items-center w-full py-3">
+              {/* Logo */}
+              <Link href="/">
+                <a href="/">
+                  <Image
+                    src="/icons/logo.png"
+                    width="160"
+                    height="55"
+                    objectFit="contain"
+                  />
+                </a>
+              </Link>
+              {/* Menu */}
               <div
-                className="cursor-pointer text-white lg:hidden flex justify-end w-full p-8"
-                onClick={() => setOpen(false)}
+                className="menu lg:hidden cursor-pointer"
+                onClick={() => setOpen((x) => !x)}
               >
-                <AiOutlineClose size={29} />
+                <HiOutlineMenuAlt3 size={40} className="text-colorText" />
               </div>
-              <div className={`nav-menu`}>
-                <ul className="main-menu pl-10 lg:pl-0 flex-col lg:flex-row lg:flex lg:items-center lg:flex-wrap gap-7 space-y-10 lg:space-y-0">
-                  <NavLi text="HOME" />
-                  <NavLi text="ABOUT US" />
-                  <NavLi text="SERVICE" />
 
-                  {/* Drop Down start */}
-                  <DropDownLI text="PAGES" />
-                  <DropDownLI text="CONTACT US" />
-                  {/* Drop Down end */}
-                  {/* Button */}
-                  <div className="hidden lg:block">
-                    <ButtonC title="GET STARTED" text="12px" />
-                  </div>
-                </ul>
+              {/* NavList */}
+              {/* bg-teal-700  bg-slate-800*/}
+              <div
+                className={`${
+                  !open &&
+                  "absolute bg-teal-700 left-[-350px] lg:left-0 transition-all duration-[200ms] ease-in-out lg:duration-[0ms] z-[1000]"
+                } absolute top-0 left-0 bg-black3 lg:relative lg:top-0 z-[999] lg:justify-between w-[350px] h-screen lg:h-auto flex-col flex lg:flex lg:w-auto lg:order-1 lg:bg-transparent transition-all duration-[1500ms] ease-in-out lg:duration-[0ms]`}
+                // id="mobile-menu-4"
+              >
+                {/* ==== Menu Close Icon ==== */}
+                <div
+                  className="cursor-pointer text-white lg:hidden flex justify-end w-full p-8"
+                  onClick={() => setOpen(false)}
+                >
+                  <AiOutlineClose size={29} />
+                </div>
+                <div className={`nav-menu`}>
+                  <ul className="main-menu pl-10 lg:pl-0 flex-col lg:flex-row lg:flex lg:items-center lg:flex-wrap gap-7 space-y-10 lg:space-y-0">
+                    <NavLi text="HOME" />
+                    <NavLi text="ABOUT US" />
+                    <NavLi text="SERVICE" />
+
+                    {/* Drop Down start */}
+                    <DropDownLI text="PAGES" />
+                    <DropDownLI text="CONTACT US" />
+                    {/* Drop Down end */}
+                    {/* Button */}
+                    <div className="hidden lg:block">
+                      <ButtonC title="GET STARTED" text="12px" />
+                    </div>
+                  </ul>
+                </div>
               </div>
+
+              {/* BackDrop */}
+              <div
+                className={`${
+                  !open &&
+                  "absolute bg-teal-700 left-[-990px] lg:left-0 transition-all duration-[1200ms] ease-in-out lg:duration-[0ms] z-[995]"
+                } absolute top-0 left-0 bg-slate-800 opacity-60 lg:relative lg:top-0 z-[994] lg:justify-between w-screen h-screen lg:h-auto flex-col flex lg:flex lg:w-auto lg:order-1 lg:bg-transparent transition-all duration-[600ms] ease-in-out lg:duration-[0ms]`}
+                onClick={() => setOpen(false)}
+              ></div>
             </div>
           </div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
+    </>
   );
 };
 
