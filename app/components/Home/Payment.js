e10430200a7c6a86906f1fdc43e4c48061167b55
programmerrafi <mdrafiprogrammer@gmail.com>
2022-12-01 19:47:49 +0600

    Payment section responsive done

--- app/components/Home/Payment.js
+++ app/components/Home/Payment.js
@@ -8,15 +8,15 @@ import { FaStripe } from "react-icons/fa";
 
 function Payment() {
   return (
-    <section className="bg-bgMain pb-20 pt-16">
+    <section className="bg-bgMain pb-14 pt-4 md:pb-20 md:pt-16">
       <div className="container">
-        <div className="flex md:flex-nowrap flex-wrap gap-32 container">
+        <div className="flex lg:flex-nowrap flex-wrap gap-8 md:gap-22 lg:gap-32">
           {/* Left Side */}
-          <div className="md:w-[40%] flex justify-center flex-col">
-            <h1 className="text-[45px] font-semibold text-colorWhite mb-8 leading-tight">
-              Support Payment <br /> From Every Plartform
+          <div className="lg:w-[40%] flex justify-center flex-col order-10 md:order-1">
+            <h1 className="md:text-[45px] text-[30px] font-semibold text-colorWhite mb-4 lg:mb-8 leading-tight lg:flex flex-col">
+              Support Payment <span>From Every Plartform</span>
             </h1>
-            <p className="text-sm text-whiteGray mb-6 leading-6">
+            <p className="text-sm text-whiteGray mb-6 leading-6 md:font-medium font-thin">
               Sed ut perspiciatis unde omnis iste natus error sit voluptatem
               accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
               quae ab illo inventore veritatis et quasi architecto beatae vitae
@@ -41,15 +41,15 @@ function Payment() {
             </div>
           </div>
           {/* Right side */}
-          <div className="w-[80%] relative mx-auto md:mx-0 md:w-[45%]">
-            <div className="absolute -left-12 bottom-0 z-10 !w-[220px] shadowBox elementor-animation-bob">
+          <div className="md:w-[80%] w-full relative mx-auto md:mx-0 lg:w-[45%] order-9 md:order-2">
+            <div className="absolute -left-12 bottom-0 z-10 !w-[220px] shadowBox elementor-animation-bob hidden md:block">
               <img
                 src="/images/Last-Operation1.png"
                 className="w-full object-contain"
               />
             </div>
             <div
-              className="w-full h-[550px]"
+              className="w-full h-[95%] lg:h-[550px]"
               style={setBackgroundImage("images/payment-img.jpg", {
                 backgroundPosition: "center center",
                 backgroundColor: "#f3f3f3",
@@ -61,7 +61,7 @@ function Payment() {
                 className="w-full h-full overflow-hidden"
               />
             </div>
-            <div className="h-[95%] p-4 absolute bottom-12 -right-32 elementor-animation-bob">
+            <div className="h-[95%] p-4 absolute bottom-12 -right-32 elementor-animation-bob hidden md:block">
               <img
                 src="/images/payment-icon-home.png"
                 className="h-full object-contain"
